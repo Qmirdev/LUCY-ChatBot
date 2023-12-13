ea5b28a26f8bfad9f397e574152c085612ad4e50
Qmir <saffaridev@gmail.com>
2023-12-14 02:34:08 +0330

    Added a new feature
    now everything logs to chat-log.txt

--- chatbot.py
+++ chatbot.py
@@ -8,4 +8,17 @@ pairs = [
 
 chat = Chat(pairs, reflections)
 
-chat.converse()
+def log_conversation(user_input, bot_response):
+  timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  with open('chat-log.txt', 'a') as f:
+    f.write("{} User: {}\n".format(timestamp, user_input))
+    f.write("{} Bot: {}\n".format(timestamp, bot_response))
+
+print("Hello, I'm Chatbot! Ask me something.")
+while True:
+  user_input = input("You: ")
+  bot_response = chat.respond(user_input)
+
+  print("Bot: {}".format(bot_response))
+
+  log_conversation(user_input, bot_response)
