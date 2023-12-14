38542170f666535611698bf64e81d3abc8fc415c
Qmir <saffaridev@gmail.com>
2023-12-14 03:46:04 +0330

    updated pairs

--- chatbot.py
+++ chatbot.py
@@ -11,13 +11,13 @@ def log_conversation(user_input, bot_response):
   timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
   with open('chat-log.txt', 'a') as f:
     f.write("{} User: {}\n".format(timestamp, user_input))
-    f.write("{} Bot: {}\n".format(timestamp, bot_response))
+    f.write("{} Lucy: {}\n".format(timestamp, bot_response))
 
-print("Hello, I'm Chatbot! Ask me something.")
+print("Hello, I'm Lucy!")
 while True:
   user_input = input("You: ")
   bot_response = chat.respond(user_input)
 
-  print("Bot: {}".format(bot_response))
+  print("Lucy: {}".format(bot_response))
 
   log_conversation(user_input, bot_response)
--- pairs.py
+++ pairs.py
@@ -1,3 +1,3 @@
 pairs = [
-    ["Hi", ["Hello", "Hey there!"]],
+    ["Hi", ["Hello", "Hey there!", "Hey there!", "What’s up?", "Howdy!", "Well", "hello!", "Greetings!", "Hiya!", "Hey, how’s it going?", "Yo!", "Hi, nice to see you!", "Hey, long time no see!", "Hi, how have you been?", "Well, well, well, look who it is!", "Hi, what’s the latest?", "Hey, what’s the haps?", "Hi, how’s life treating you?", "Well, hello stranger!", "Hey, fancy meeting you here!", "Hi, how’s your day shaping up?", "Hey, how’s everything in your world?", "Hi, it’s good to hear from you!", "Well, hi there, sunshine!", "Hey, what’s the good word?", "Hi, how’s your week going so far?", "Hey, how’s the weather treating you?", "Hi, what’s new and exciting?", "Well, hello, my friend!", "Hey, how’s the day treating you?", "Hi, what’s the plan for today?", "Hey, how’s life on your end?", "Hi, hope you’re having a great day!"]],
 ]
